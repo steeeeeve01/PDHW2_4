2ed4be315fb518e21cd95997d40df872f255e516
steeeeeve01 <136960831+steeeeeve01@users.noreply.github.com>
2025-09-16 01:02:26 +0800

    Initial Commit

---
+++ PDHW2_4/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  PDHW2_4
+//
+//  Created by 簡紹益 on 2025/9/16.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
